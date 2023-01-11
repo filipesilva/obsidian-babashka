f4055cdd3656c22734748f72d4baef3e3bde22e2
Filipe Silva <filipematossilva@gmail.com>
2023-01-11 16:48:54 +0000

    feat: base functionality

--- main.ts
+++ main.ts
@@ -1,81 +1,130 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, EditorPosition, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { exec } from 'child_process';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface Codeblock {
+	lang: string,
+	code: string,
+	insertPos: EditorPosition,
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+function getCodeblockForCursor(editor: Editor): Codeblock | null {
+	const src = editor.getValue();
+	const cursor = editor.getCursor();
+	const cljRe = /^```(clojure|clojurescript)$\s([\s\S]*?)^```$/gmd;
+	const cljCodeblocks = Array.from(src.matchAll(cljRe));
+	const cursorOffset = editor.posToOffset(cursor);
+	const insideMatch = offset => match => match.indices[0][0] <= offset && offset <= match.indices[0][1];
+	const match = cljCodeblocks.find(insideMatch(cursorOffset));
+
+	if (match) {
+		return {
+			lang: match[1],
+			code: match[2],
+			insertPos: editor.offsetToPos(match.indices[2][1]),
+		}
+	} else {
+		return null;
+	}
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+function validateSettingsForCodeblock(codeblock: Codeblock, settings: PluginSettings) {
+	const { lang } = codeblock;
+	const { bbPath, nbbPath, nodePath } = settings;
+	const errors = [];
+
+	if (lang == 'clojure' && !bbPath) {
+		new Notice('Please set Babashka path in settings');
+		return false;
+	}
+
+	if (lang == 'clojurescript' && (!nbbPath || !nodePath)) {
+		new Notice('Please set both Node Babashka and Node paths in settings');
+		return false;
+	}
+
+	return true;
+}
+
+function executeCodeblock(codeblock: Codeblock, vaultPath: string, editor: Editor, settings: PluginSettings) {
+	const { lang, code, insertPos } = codeblock;
+	const { bbPath, nbbPath, nodePath, bbDir } = settings;
+
+	if (validateSettingsForCodeblock(codeblock, settings)) {
+		const bin = lang == 'clojure' ? bbPath : `${nodePath} ${nbbPath}`;
+		const ext = lang == 'clojure' ? 'clj' : 'cljs';
+		const pluginCwd = `${vaultPath}/${bbDir}`;
+
+		const codeShellStr = code.replaceAll("\"", "\\\"");
+		const cmd = `${bin} -e "${codeShellStr}"`;
+
+		console.debug(`babashka executing: \`${cmd}\` on ${pluginCwd}`);
+		// TODO: save process for kill?
+		// TODO: some max lines
+		const p = exec(
+			cmd,
+			{ cwd: `${pluginCwd}` },
+			(err, stdout, stderr) => {
+				if (err) {
+					const errorMsg = 'Error during execution:\n' + err;
+					console.error(errorMsg)
+					new Notice(errorMsg, 5000);
+					return;
+				}
+
+				if (stderr) {
+					const errorMsg = 'stderr during execution:\n' + stderr;
+					console.error(errorMsg)
+					new Notice(errorMsg, 5000);
+					return;
+				}
+
+				if (stdout) {
+					const outputAsComments = stdout.trim().replaceAll(/^/gm, ';; ');
+					editor.replaceRange(`\n${outputAsComments}\n`, insertPos, insertPos);
+
+				}
+				console.debug(`babashka stdout:\n${stdout}`);
+				console.debug(`babashka stderr:\n${stderr}`);
+			});
+	}
+}
+
+interface PluginSettings {
+	bbDir: string,
+	bbPath: string,
+	nbbPath: string,
+	nodePath: string,
+}
+
+const DEFAULT_SETTINGS: PluginSettings = {
+	bbDir: 'babashka',
+	bbPath: '',
+	nbbPath: '',
+	nodePath: '',
+}
+
+export default class BabashkaPlugin extends Plugin {
+	settings: PluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addSettingTab(new SettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: 'obsidian-babashka-execute-codeblock',
+			name: 'Execute codeblock',
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				const codeblock = getCodeblockForCursor(editor);
+				if (codeblock) {
+					const vaultPath = this.app.vault.adapter.getBasePath();
+					executeCodeblock(codeblock, vaultPath, editor, this.settings);
+				} else {
+					new Notice('No clojure(script) codeblock found');
 				}
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,47 +140,38 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingTab extends PluginSettingTab {
+	plugin: BabashkaPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: BabashkaPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
+	addSetting(name: string, desc: string, placeholder: string, k: keyof PluginSettings) {
+		const { containerEl } = this;
+		new Setting(containerEl)
+			.setName(name)
+			.setDesc(desc)
+			.addText(text => text
+				.setPlaceholder(placeholder)
+				.setValue(this.plugin.settings[k])
+				.onChange(async (value) => {
+					this.plugin.settings[k] = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Paths' });
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		this.addSetting('Vault Babashka dir', 'Relative path to babashka dir from vault root. Babashka will be run from this dir. You can put bb.edn, nbb.edn, and package.json files there to use dependencies.', '', 'bbDir');
+		this.addSetting('Babashka path', 'Absolute path to babashka', 'run `which bb` to see it', 'bbPath');
+		this.addSetting('Node Babashka path', 'Absolute path to nbb', 'run `which nbb` to see it', 'nbbPath');
+		this.addSetting('Node path', 'Absolute path to node', 'run `which nbb` to see it', 'nodePath');
 	}
 }
